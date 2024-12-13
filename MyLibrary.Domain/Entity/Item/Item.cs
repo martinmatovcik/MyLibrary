8b636f37a9d2912eb78dbee985f2e524ec439fa2
martinmatovcik <martin.matovcik@stratox.cz>
2024-12-13 23:05:06 +0100

    IsStatus() methos

--- MyLibrary.Domain/Entity/Item/Item.cs
+++ MyLibrary.Domain/Entity/Item/Item.cs
@@ -9,19 +9,25 @@ public abstract class Item : Abstraction.Entity
     public List<BorrowingDetails> BorrowingHistory { get; private set; } = [];
 
     public void SetName(string newName) => Name = newName;
-    
+
     public void SetDescription(string newDescription) => SetDescriptionInternal(newDescription);
 
     public void RemoveDescription() => SetDescriptionInternal(null);
 
     private void SetDescriptionInternal(string? newDescription) => Description = newDescription;
 
+    public bool IsAvailable() => IsStatus(Status.Available);
+
     public void SetAvailable() => SetStatus(Status.Available);
 
     public void SetNotAvailable() => SetStatus(Status.NotAvailable);
 
+    public bool IsReserved() => IsStatus(Status.Reserved);
+
     public void SetReserved() => SetStatus(Status.Reserved);
-    
+
+    private bool IsStatus(Status status) => Status == status;
+
     private void SetStatus(Status newStatus) => Status = newStatus;
 
     //Borrow() - pozicanie
