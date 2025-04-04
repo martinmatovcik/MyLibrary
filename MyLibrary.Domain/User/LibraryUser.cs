b0805df17a8411db0d5a525b8bda7be36f553ad3
martinmatovcik <martin.matovcik@stratox.cz>
2025-04-04 08:23:36 +0200

    feat(ML-1): switched UserDetails empty from static property to method. Reason: EF owned types

--- MyLibrary.Domain/User/LibraryUser.cs
+++ MyLibrary.Domain/User/LibraryUser.cs
@@ -2,7 +2,7 @@ namespace MyLibrary.Domain.User;
 
 public class LibraryUser : Abstraction.Entity
 {
-    public UserDetails Details { get; private set; } = UserDetails.Empty;
+    public UserDetails Details { get; private set; } = UserDetails.CreateEmpty();
     public List<Item.Item> OwnedItems { get; private set; } = [];
     public List<Order.Order> Orders { get; private set; } = [];
 
@@ -17,5 +17,5 @@ public class LibraryUser : Abstraction.Entity
         Orders = orders;
     }
 
-    public static LibraryUser CreateEmpty(UserDetails? userDetails = null) => new(userDetails ?? UserDetails.Empty, [], []);
+    public static LibraryUser CreateEmpty(UserDetails? userDetails = null) => new(userDetails ?? UserDetails.CreateEmpty(), [], []);
 }
